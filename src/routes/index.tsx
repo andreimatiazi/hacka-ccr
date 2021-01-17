8ebeba91c489af1dcd2b008d4952dda12d182c82
andreimatiazi <andreimatiazi@users.noreply.github.com>
2021-01-17 17:29:24 -0300

    Atualização das rotas da aplicação

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,11 +2,19 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 
 import Login from "../pages/Login";
+import Cadastro from "../pages/Cadastro";
+import Cursos from "../pages/Cursos";
+import Homepage from "../pages/Homepage";
+import Marketing from "../pages/Marketing";
 
 const Routes: React.FC = () => {
   return (
     <Switch>
       <Route exact path="/" component={Login} />
+      <Route exact path="/cadastro" component={Cadastro} />
+      <Route exact path="/cursos" component={Cursos} />
+      <Route exact path="/inicio" component={Homepage} />
+      <Route exact path="/marketing" component={Marketing} />
     </Switch>
   );
 };

112ee17bc9938fc4104a7e0f4169f3b1912d5403
andreimatiazi <andreimatiazi@users.noreply.github.com>
2021-01-17 17:27:15 -0300

    Adicionada pagina de cursos

---
+++ src/pages/Cursos/index.tsx
@@ -0,0 +1,217 @@
+import React, { useCallback } from "react";
+import { useHistory } from "react-router-dom";
+
+import Col from "react-bootstrap/Col";
+import Image from "react-bootstrap/Image";
+import Button from "react-bootstrap/Button";
+
+import WebLayout from "../../layouts/CardWeb";
+import LoginForm from "../../components/LoginForm";
+
+import Web from "../../assets/web-development.svg";
+import Marketing from "../../assets/digital-marketing.svg";
+import Venda from "../../assets/venda.svg";
+import Financas from "../../assets/financas.svg";
+import DataScience from "../../assets/data-science.svg";
+import Row from "react-bootstrap/esm/Row";
+import blackWomen from "../../assets/blackwomen.png";
+
+interface ICardLayoutProps {
+  navigate(route: string): void;
+}
+
+const Homepage: React.FC<ICardLayoutProps> = ({ navigate }) => {
+  return (
+    <WebLayout>
+      <Col
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <h1>O que você quer aprender hoje?</h1>
+      </Col>
+      <Row>
+        <Col xs={1} md={1}></Col>
+        <Col xs={2} md={2}>
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              flexDirection: "column",
+              margin: "0 2rem",
+            }}
+          >
+            <button
+              style={{
+                border: "none",
+                width: "7rem",
+                height: "7rem",
+                borderRadius: "50%",
+                background: "#FFE548",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+              onClick={() => navigate("/agendamento-de-consulta")}
+            >
+              <Image src={Web} width="50%" />
+            </button>
+            <p style={{ marginTop: 13, textAlign: "center" }}>
+              Desenvolvimento
+            </p>
+          </div>
+        </Col>
+        <Col xs={2} md={2}>
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              flexDirection: "column",
+              margin: "0 2rem",
+            }}
+          >
+            <button
+              style={{
+                border: "none",
+                width: "7rem",
+                height: "7rem",
+                borderRadius: "50%",
+                background: "#FFE548",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+              onClick={() => navigate("/agendamento-de-consulta")}
+            >
+              <Image src={Marketing} width="50%" />
+            </button>
+            <p style={{ marginTop: 13, textAlign: "center" }}>Marketing</p>
+          </div>
+        </Col>
+        <Col xs={2} md={2}>
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              flexDirection: "column",
+              margin: "0 2rem",
+            }}
+          >
+            <button
+              style={{
+                border: "none",
+                width: "7rem",
+                height: "7rem",
+                borderRadius: "50%",
+                background: "#FFE548",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+              onClick={() => navigate("/agendamento-de-consulta")}
+            >
+              <Image src={Venda} width="50%" />
+            </button>
+            <p style={{ marginTop: 13, textAlign: "center" }}>Vendas</p>
+          </div>
+        </Col>
+        <Col xs={2} md={2}>
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              flexDirection: "column",
+              margin: "0 2rem",
+            }}
+          >
+            <button
+              style={{
+                border: "none",
+                width: "7rem",
+                height: "7rem",
+                borderRadius: "50%",
+                background: "#FFE548",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+              onClick={() => navigate("/agendamento-de-consulta")}
+            >
+              <Image src={Financas} width="50%" />
+            </button>
+            <p style={{ marginTop: 13, textAlign: "center" }}>Finanças</p>
+          </div>
+        </Col>
+        <Col xs={2} md={2}>
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              flexDirection: "column",
+              margin: "0 2rem",
+            }}
+          >
+            <button
+              style={{
+                border: "none",
+                width: "7rem",
+                height: "7rem",
+                borderRadius: "50%",
+                background: "#FFE548",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+              onClick={() => navigate("/agendamento-de-consulta")}
+            >
+              <Image src={DataScience} width="50%" />
+            </button>
+            <p style={{ marginTop: 13, textAlign: "center" }}>Data Science</p>
+          </div>
+        </Col>
+        <Col xs={1} md={1}></Col>
+      </Row>
+      <Col
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <h2 style={{ fontSize: 30, paddingRight: 100 }}>
+          Ainda não sabe o que quer aprender?
+        </h2>{" "}
+        <Row style={{ marginTop: 15 }}>
+          <Col>
+            <Button
+              style={{
+                backgroundColor: "#F21B3F",
+                borderColor: "#E5E5E5",
+                color: "#fff",
+                borderRadius: 35,
+                padding: 8,
+                paddingInline: 50,
+                borderWidth: 1,
+                fontSize: 24,
+              }}
+              variant="primary"
+              type="submit"
+              block
+            >
+              Descubra!
+            </Button>
+          </Col>
+        </Row>
+      </Col>
+    </WebLayout>
+  );
+};
+
+export default Homepage;
